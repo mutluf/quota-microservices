3e1a4dbe3ab0e33187bd7ca983c0c8330916b2fd
Fatma <fmutlu3002@gmail.com>
2023-12-12 14:25:58 +0300

    quote service

--- src/Services/QuoteService/Quote.API/Services/QuoteService.cs
+++ src/Services/QuoteService/Quote.API/Services/QuoteService.cs
@@ -1,4 +1,5 @@
-﻿using Quote.API.Dtos;
+﻿using AutoMapper;
+using Quote.API.Dtos;
 using Quote.API.Repositories;
 
 namespace Quote.API.Services
@@ -6,35 +7,51 @@ namespace Quote.API.Services
     public class QuoteService : IQuoteService
     {
         private readonly IQuoteRepository _quoteRepository;
+        private readonly IMapper _mapper;
 
-        public QuoteService(IQuoteRepository quoteRepository)
+        public QuoteService(IQuoteRepository quoteRepository, IMapper mapper)
         {
             _quoteRepository = quoteRepository;
+            _mapper = mapper;
         }
 
-        public Task<bool> CreateAsync(QuoteDto quote)
+        public async Task<bool> CreateAsync(QuoteDto quoteDto)
         {
-            throw new NotImplementedException();
+            Entities.Quote quote = _mapper.Map<Entities.Quote>(quoteDto);
+            bool result = await _quoteRepository.CreateQuoteAsync(quote);
+            await _quoteRepository.SaveAsync();
+
+            return result;
         }
 
-        public void Delete(int id)
+        public async void Delete(int id)
         {
-            throw new NotImplementedException();
+            _quoteRepository.DeleteQuote(id);
+            await _quoteRepository.SaveAsync();
         }
 
         public List<QuoteDto> GetAll()
         {
-            throw new NotImplementedException();
+            List<Entities.Quote> quotes = _quoteRepository.GetAll();
+            List<QuoteDto> datas = _mapper.Map<List<QuoteDto>>(quotes);
+
+            return datas;
         }
 
-        public Task<QuoteDto> GetByIdAsync(int id)
+        public async Task<QuoteDto> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            Entities.Quote quote= await _quoteRepository.GetByIdAsync(id);
+            QuoteDto data = _mapper.Map<QuoteDto>(quote);
+
+            return data;
         }
 
         public bool Update(QuoteDto quote)
         {
-            throw new NotImplementedException();
+            Entities.Quote data = _mapper.Map<Entities.Quote>(quote);
+            bool result = _quoteRepository.UpdateQuote(data);
+
+            return result;
         }
     }
 }
